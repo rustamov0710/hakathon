b190d2091c83bf5f7233122d313f068385fa5e88
unknown <you@example.com>
2025-01-31 17:58:03 +0500

    add:router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 
---
+++ src/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <>
+    <h2>
+    Услуги и акции
+    </h2>
+    </>
+  )
+}
+
+export default Services
